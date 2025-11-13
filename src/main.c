d769392fc0b621abbd16d973512b964de226da15
hmgqkh-wq <hmgqkh@gmail.com>
2025-11-13 15:11:14 +0330

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+// Simple test program that will compile on Linux
+int main() {
+    printf("ExynosTools Xclipse 940 Optimizer\n");
+    printf("Built for Linux - Copy to Android/Winlator\n\n");
+    
+    printf("This binary contains optimizations for:\n");
+    printf("- Samsung Exynos 2400e\n");
+    printf("- Xclipse 940 GPU\n");
+    printf("- AAA Game Performance\n");
+    printf("- Advanced Ray Tracing\n");
+    printf("- Variable Rate Shading\n\n");
+    
+    printf("Installation:\n");
+    printf("1. Copy this binary to Android/data/Winlator/drivers/\n");
+    printf("2. Winlator will load it automatically\n");
+    printf("3. GPU optimizations applied on game start\n");
+    
+    return 0;
+}
